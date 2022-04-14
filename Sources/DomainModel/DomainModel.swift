5a40eb0aa2989301b910f03e547cfe8ef67fa0a7
omniFatal <matthew.t.karyadi@gmail.com>
2022-04-14 11:40:44 -0700

    Write Money.convert

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -9,6 +9,39 @@ struct DomainModel {
 // Money
 //
 public struct Money {
+    let amount: Int
+    let currency: String
+    
+    func convert(_ curr: String) -> Money {
+        var returnAmount: Double
+        if curr != self.currency {
+            switch self.currency {
+            // Convert the money to USD first
+            case "GBP":
+                returnAmount = Double(self.amount) * 2
+            case "EUR":
+                returnAmount = (Double(self.amount) * 2) / 3
+            case "CAD":
+                returnAmount = (Double(self.amount) * 4) / 3
+            default:
+                returnAmount = Double(self.amount)
+            }
+            switch curr {
+            // Convert to the desired currency and return the Money as a result
+            case "GBP":
+                return Money(amount: Int((returnAmount / 2).rounded()), currency: "GBP")
+            case "EUR":
+                return Money(amount: Int((returnAmount * 1.5).rounded()), currency: "EUR")
+            case "CAD":
+                return Money(amount: Int((returnAmount * 1.25).rounded()), currency: "CAD")
+            default:
+                return Money(amount: Int(returnAmount.rounded()), currency: "USD")
+            }
+        } else {
+            return self
+        }
+    }
+    
 }
 
 ////////////////////////////////////
